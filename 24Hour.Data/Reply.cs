b469f97f664f7efc48f685e91ea8801b811fa218
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-04 15:52:42 -0400

    pulling from develop

---
+++ 24Hour.Data/Reply.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public  class Reply
+    {
+        [Key]
+        public int ReplyId { get; set; }
+
+        [Required]
+        public string RContent { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ 24Hour.Models/ReplyEdit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Models
+{
+    public class ReplyEdit
+    {
+        public int ReplyId { get; set; }
+        public string RContent { get; set; }
+
+    }
+}
